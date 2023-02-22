879cb134cb7859f890d4ddb4900efc758dc14466
Devin Benninghoven <devinbenninghoven@gmail.com>
2023-02-21 16:59:30 -0800

    started rough framework for main.cc

--- main.cc
+++ main.cc
@@ -1,8 +1,40 @@
+// Devin Benninghoven
+
+
+
 #include <iostream>
+#include <set>
+#include <string>
 using namespace std;
 
+
+std::set<string> setOfCommands = {
+        "dir",
+        "help",
+        "vol",
+        "path",
+        "tasklist",
+        "notepad",
+        "echo",
+        "color",
+        "ping"
+};
+
+string greeting = "Welcome to myShell";
+string prompt = "==> ";
+string exitMessage = "Thanks for using myShell!";
 int main(){
-    cout << "Hello world\n";
-    cout << "Test 2\n";
+    cout << greeting << "\n";
+    while (true){
+        cout << prompt;
+        string input = "";
+        cin >> input;
+        if (input == "exit" or input == "quit") break;
+
+
+
+    }
+
+    cout << exitMessage << "\n";
     return 0;
 }
